e59de4229f1e6250e8ad3390a9d746a1554d16ac
ValeriiCSharp <114076774+ValeriiCSharp@users.noreply.github.com>
2025-04-29 17:14:04 +0300

    TakeOnKey

--- ThirdRoom/Assets/CodeBase/Obtainer.cs
+++ ThirdRoom/Assets/CodeBase/Obtainer.cs
@@ -6,7 +6,6 @@ using CodeBase.Inventory.Controller;
 using DG.Tweening;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Zenject;
 
 namespace CodeBase
@@ -22,14 +21,14 @@ namespace CodeBase
         [SerializeField] private bool _isScalingObject;
         [SerializeField] private bool _isDeductiveObject;
         [SerializeField] private bool _isRotatingObject;
-        
+
         [Header("Object Types")]
         [SerializeField] private bool _isLetter;
         [SerializeField] private bool _isStick;
-        
+
         [Space(10)]
         [SerializeField] private float _returnSpeed;
-        
+
         public Vector3 StartPosition { get; set; }
         public Vector3 StartRotation { get; set; }
 
@@ -37,28 +36,31 @@ namespace CodeBase
 
         private PlayerPrefab _playerPrefab;
         private InventoryController _inventoryController;
-        
+
+        private bool _isInspecting;
+        private Item _currentItem;
+        private string _currentItemPath;
+
         [Inject]
         public void Construct(PlayerPrefab playerPrefab, InventoryController inventoryController)
         {
             _playerPrefab = playerPrefab;
             _inventoryController = inventoryController;
         }
-        
+
         private void OnValidate()
         {
             _objectRotation = FindFirstObjectByType<ObjectRotation>();
 #if UNITY_EDITOR
-
             EditorUtility.SetDirty(this);
 #endif
         }
 
         private void Start()
         {
-            StartPosition = gameObject.transform.position;
-            StartRotation = gameObject.transform.rotation.eulerAngles;
-            
+            StartPosition = transform.position;
+            StartRotation = transform.rotation.eulerAngles;
+
             _obtainerUI.OnDestroyRequested += OnDestroyRequested;
             _obtainerUI.OnMoveToStashRequested += OnMoveToStashRequested;
         }
@@ -68,84 +70,88 @@ namespace CodeBase
             _obtainerUI.OnDestroyRequested -= OnDestroyRequested;
             _obtainerUI.OnMoveToStashRequested -= OnMoveToStashRequested;
         }
-        
+
+        private void Update()
+        {
+            if (_isInspecting && Input.GetKeyDown(KeyCode.Return))
+            {
+                ConfirmObtain();
+            }
+        }
+
         protected override void OnInteract()
-            => Obtain();
+        {
+            Obtain();
+        }
 
         private void Obtain()
         {
-            
+            _objectRotation.Activate(transform, _isScalingObject, MoveToCameraPlan);
+            _playerPrefab.BlockInput();
+            RotateToCamera();
+
             if (_isStick)
             {
-                _objectRotation.Activate(transform, _isScalingObject, MoveToCameraPlan);
-                _playerPrefab.BlockInput();
-                RotateToCamera();
-                
-                _inventoryController.AddItem(Resources.Load<Item>("InventoryObjects/Stick"));
-                
-                _obtainerUI.Display(transform, LoadItem("InventoryObjects/Stick"));
+                _currentItemPath = "InventoryObjects/Stick";
+                _currentItem = LoadItem(_currentItemPath);
+                _obtainerUI.Display(transform, _currentItem);
             }
-            if (_isLetter)
+            else if (_isLetter)
             {
-                _objectRotation.Activate(transform, _isScalingObject, MoveToCameraPlan);
-                _playerPrefab.BlockInput();
-                RotateToCamera();
-                
-                _inventoryController.AddItem(Resources.Load<Item>("InventoryObjects/Letter"));
-                
-                _obtainerUI.Display(transform, LoadItem("InventoryObjects/Stick"));
+                _currentItemPath = "InventoryObjects/Letter";
+                _currentItem = LoadItem(_currentItemPath);
+                _obtainerUI.Display(transform, _currentItem);
             }
-            else
-            {
-                _objectRotation.Activate(transform, _isScalingObject);
-                _playerPrefab.BlockInput();
-                RotateToCamera();
-            }
+
+            _isInspecting = true;
+        }
+
+        private void ConfirmObtain()
+        {
+            if (_currentItem == null) return;
+
+            _inventoryController.AddItem(_currentItem);
+            _isInspecting = false;
+            _playerPrefab.UnblockInput();
+            Debug.Log($"{_currentItem.name} added to inventory.");
         }
 
         private void MoveToCameraPlan()
         {
-            if(Camera.main == null) return;
-            transform.DOLocalMove(new Vector3(0,0,-0.62f), 1f);
+            if (Camera.main == null) return;
+            transform.DOLocalMove(new Vector3(0, 0, -0.62f), 1f);
         }
-        
+
         private void RotateToCamera()
         {
             if (Camera.main == null) return;
 
             Vector3 directionToCamera = Camera.main.transform.position - transform.position;
-
             directionToCamera.y = 0;
 
             Quaternion targetRotation = Quaternion.LookRotation(directionToCamera);
-
             transform.DORotateQuaternion(targetRotation, 1f).SetEase(Ease.InOutSine);
-        }
-
-        private void ZoomCamera()
-        {
-            transform.DOScale(transform.localScale * 1.5f, 0.4f).SetEase(Ease.InOutSine);
         }
 
         private Item LoadItem(string path)
         {
-            Item item = Resources.Load<Item>("InventoryObjects/Letter");
-            return item;
+            return Resources.Load<Item>(path);
         }
-        
+
         private void OnMoveToStashRequested(Transform target)
         {
-            print("MOVE TO SHTASH!!!");
             _objectRotation.Deactivate();
             target.SetParent(null);
             target.DORotate(StartRotation, 1f);
-            target.DOMove(_trailerDisposePosterPosition.position, 0.5f).OnComplete(() => Destroy(target.gameObject));
+            target.DOMove(_trailerDisposePosterPosition.position, 0.5f)
+                  .OnComplete(() => Destroy(target.gameObject));
+
             if (_isDeductiveObject)
             {
                 OnDeductiveObjectClaimed?.Invoke();
             }
         }
-        
+
         private void OnDestroyRequested()
         {
             _objectRotation.Deactivate();
